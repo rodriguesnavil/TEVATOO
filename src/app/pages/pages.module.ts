ded6358aa38b1dc573eb716c4937b09129affa00
Navil Rodrigues <rodriguesnavil@gmail.com>
2022-06-24 20:39:46 +0530

    cleaning...

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,22 +3,20 @@ import { NbMenuModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
-    ECommerceModule,
     MiscellaneousModule,
   ],
   declarations: [
     PagesComponent,
+    DashboardComponent,
   ],
 })
 export class PagesModule {
