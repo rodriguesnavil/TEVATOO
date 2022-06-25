aab50c987302f00f5bf47a625b218c495a4f9cde
Navil Rodrigues <rodriguesnavil@gmail.com>
2022-06-25 22:55:34 +0530

    change footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,14 +5,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b>Navil Rodrigues</b> &copy; {{ today | date: 'yyyy' }}.
+      Created with ♥ by <b>Mallikarjun & Navil</b> &copy; {{ today | date: 'yyyy' }}.
     </span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
   `,
 })
 export class FooterComponent {
